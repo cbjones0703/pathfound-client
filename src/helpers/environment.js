04c2619d2edb08c6fdc784112a765a3d86f8950d
unknown <cbjones0703@gmail.com>
2018-11-13 11:07:24 -0500

    third client commit

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,7 +7,7 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000';
         break;
     //this is the deployed react application
-    case 'https://pathfoundclient.herokuapp.com':
+    case 'pathfoundclient.herokuapp.com':
     //this is the full url of your deployed API
         APIURL = 'https://pathfoundserver.herokuapp.com'
 }
